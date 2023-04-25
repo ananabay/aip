219e247fdb4e60fae86aa0647ae306e71dbe7898
Ruth Rosenblum <ruth.rosenblum@posh.tech>
2023-04-25 12:39:41 -0400

    added limit arg

--- train_model.py
+++ train_model.py
@@ -1,5 +1,6 @@
 import model as m
 
+import torch
 from torch.optim.lr_scheduler import LambdaLR
 from sklearn.pipeline import Pipeline
 from skorch.hf import HuggingfacePretrainedTokenizer
@@ -17,7 +18,7 @@ import numpy as np
 
 csv.field_size_limit(sys.maxsize)
 
-def get_X_y_from_file(filename, label_dict=None, limit=None):
+def get_X_y_from_file(filename, label_column=3, label_dict=None, limit=None):
     X = []
     y = []
     with open(filename, 'r') as f:
@@ -27,7 +28,7 @@ def get_X_y_from_file(filename, label_dict=None, limit=None):
         else:
             subsample = list(tsv_reader)
         for line in tqdm(subsample):
-            y.append(line[3])
+            y.append(line[label_column])
             sent = line[0]
             tokens = sent.split()
             sent = ' '.join(tokens)
@@ -37,10 +38,19 @@ def get_X_y_from_file(filename, label_dict=None, limit=None):
     y = np.array([label_dict[l] if l in label_dict else '-1' for l in y])
     return X, y, label_dict 
 
+def get_device(no_mps=False):
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    elif torch.has_mps and not no_mps:
+        return torch.device("mps")
+    else:
+        return torch.device("cpu")
 
 @click.command()
-@click.option("--batch-size", default=32, help='batch size (default 32)')
+@click.option("--batch-size", default=64, help='batch size (default 32)')
 @click.option("--max-epochs", default=10, help='number of epochs')
+@click.option("--predict-level", default='author', type=click.Choice(["author", "time_period", "dialect"]), help='the label to predict')
+@click.option("--limit", default=None, help='max # of examples to train on')
 @click.option("--train-file", type=click.Path(readable=True), required=True, help='path for training .tsv file')
 @click.option("--dev-file", type=click.Path(readable=True), required=False, help='path for dev/test .tsv file')
 @click.option("--log-file", default='log.txt', help='path for .txt file for logging results')
@@ -48,7 +58,7 @@ def get_X_y_from_file(filename, label_dict=None, limit=None):
 @click.option("--model-name", type=click.Choice(["dan", "lstm", "bilstm", "cnn", "bert"]), default='bert', help='model name')
 @click.option("--llm-model", type=click.Choice(["bert", "distilbert"]), default="distilbert", help='pretrained model'\
     ' to use if model-name is "bert"')
-def main(batch_size, max_epochs,
+def main(batch_size, max_epochs, predict_level, limit,
     train_file, dev_file, log_file,
     lr, model_name, llm_model,
 ):
@@ -61,7 +71,16 @@ def main(batch_size, max_epochs,
     # https://huggingface.co/scikit-learn/skorch-text-classification/blob/main/train.py
     if model_name == 'bert':
 
-        X_train, y_train, label_dict = get_X_y_from_file(train_file, limit=1000)
+        if predict_level == 'author':
+            column = 3
+        elif predict_level == 'time_period':
+            column = 2
+        else: 
+            column = 1
+
+        if limit:
+            limit = int(limit)
+        X_train, y_train, label_dict = get_X_y_from_file(train_file, limit=limit, label_column=column)
         num_training_steps = max_epochs * (len(X_train) // batch_size + 1)
 
         def lr_schedule(current_step):
@@ -81,6 +100,7 @@ def main(batch_size, max_epochs,
                 criterion=nn.CrossEntropyLoss,
                 batch_size=batch_size,
                 iterator_train__shuffle=True,
+                device=get_device(),
                 callbacks=[
                     f1_micro, f1_macro,
                     LRScheduler(LambdaLR, lr_lambda=lr_schedule, step_every='batch'),
