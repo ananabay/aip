d7e530d8e413b5d914e0344213c3363211c2fbb4
Brendayy <reginabrendayy@gmail.com>
2023-05-12 23:33:01 -0400

    adding documentations

--- preprocess_data.py
+++ preprocess_data.py
@@ -22,12 +22,15 @@ def mask_entities(text, mask):
     return " ".join([mask if t.ent_type_ and t.ent_type_ in ENTITIES_TO_MASK else t.text for t in doc])
 
 
+# preprocessing txt file and split paragraphs
 def preprocess_file(filename, out_file, mask_ner, dialect, period, author):
     book_title = filename.split('-')[-1].strip().replace('.txt','')
     with open(filename, 'r', errors='ignore') as f:
         text = ''.join(f.readlines())
-    if text.count('\n\n') > 10:
-        paragraphs = text.split('\n\n')
+    # if there are more 10 instances of double \n\n, we assume this text separate paragraphs by \n\n
+    if text.count('\n\n') > 10: 
+        paragraphs = text.split('\n\n') 
+    # otherwise we assume the paragraphs are separated by \n
     else:
         paragraphs = text.split('\n')
     print(f'{book_title}: {len(paragraphs)} paragraphs')
@@ -35,8 +38,10 @@ def preprocess_file(filename, out_file, mask_ner, dialect, period, author):
         writer = csv.writer(f, delimiter='\t', lineterminator='\n')
         for par in tqdm(paragraphs):
             par = ' '.join(par.replace('\n', ' ').split())
+            # check if masking is needed
             if mask_ner:
                 par = mask_entities(par, MASK)
+            # if the text only contains numbers and punctuations, we ignore them
             if par.strip().translate(dict([(c, '') for c in IGNORE_CHARS])) and len(par.strip())>10:
                 writer.writerow([par, dialect, period, author, book_title])
 
@@ -48,6 +53,8 @@ def preprocess_file(filename, out_file, mask_ner, dialect, period, author):
 @click.option("--dialect", default=None, help=f'if passing in just one .txt file, dialect')
 @click.option("--time-period", default=None, help=f'if passing in just one .txt file, time period')
 @click.option("--author", default=None, help=f'if passing in just one .txt file, author')
+
+
 def main(input_file, output_file, mask_named_entities, dialect, time_period, author):
     if not os.path.isfile(output_file):
         with open(output_file, 'w') as f:
